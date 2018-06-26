5c198063ca86b2bc4775564967bb80c31aee3ea7
Septdir <septdir@gmail.com>
2018-06-26 10:46:50 +0300

    Fix front_created check

--- com_prototype/site/models/form.php
+++ com_prototype/site/models/form.php
@@ -402,7 +402,7 @@ class PrototypeModelForm extends PrototypeModelItem
 					->select(array('id', 'title', 'alias', 'front_created', 'icon'))
 					->from('#__prototype_categories')
 					->where('parent_id = ' . (int) $pk)
-					->where('front_created = 1')
+					->where('front_created > 0')
 					->order('lft ASC');
 
 				// Filter by published state.
