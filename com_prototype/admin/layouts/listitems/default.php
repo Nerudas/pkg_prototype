fc3aa99a559a70168a8adfc296053b4333689941
Septdir <septdir@gmail.com>
2018-07-02 13:31:42 +0300

    Add listitem_layout to forms

---
+++ com_prototype/admin/layouts/listitems/default.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package    Prototype Component
+ * @version    1.0.4
+ * @author     Nerudas  - nerudas.ru
+ * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
+ * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ * @link       https://nerudas.ru
+ */
+
+defined('_JEXEC') or die;
+
+extract($displayData);
+
+/**
+ * Layout variables
+ * -----------------
+ * @var   Registry $item      Item data
+ * @var   Registry $extra     Item extra data
+ * @var   Registry $category  Category data
+ * @var   Registry $placemark Placemark data
+ */
+
+echo '<pre>', print_r($item, true), '</pre>';
+echo '<pre>', print_r($extra, true), '</pre>';
+echo '<pre>', print_r($category, true), '</pre>';
+echo '<pre>', print_r($placemark, true), '</pre>';
