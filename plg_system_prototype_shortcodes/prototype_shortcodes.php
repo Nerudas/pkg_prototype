288980f7f526f6886cedb8095f351d699bac4f23
Septdir <septdir@gmail.com>
2018-11-17 15:28:44 +0300

    Change version to 1.3.3

--- plg_system_prototype_shortcodes/prototype_shortcodes.php
+++ plg_system_prototype_shortcodes/prototype_shortcodes.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package    System - Prototype Shortcodes Plugin
- * @version    1.3.2
+ * @version    1.3.3
  * @author     Nerudas  - nerudas.ru
  * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
  * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
