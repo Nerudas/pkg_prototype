dae4a10db02bfd4df00c0a5b1885f5d3a3e5b84c
Septdir <septdir@gmail.com>
2019-01-29 20:15:38 +0300

    Change version to 1.4.3

--- plg_sitemap_prototype/prototype.php
+++ plg_sitemap_prototype/prototype.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package    Sitemap - Prototype Plugin
- * @version    1.4.2
+ * @version    1.4.3
  * @author     Nerudas  - nerudas.ru
  * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
  * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
